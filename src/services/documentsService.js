4a5476146daa72da36b6e69d95b29840926b9249
mhid-rethink <matheus.dias@rethink.dev>
2024-04-16 17:51:26 -0300

    feat: create document

--- src/services/documentsService.js
+++ src/services/documentsService.js
@@ -2,39 +2,53 @@ import { makeError } from "../middlewares/errorHandler.js";
 import documentRepository from "../repositories/documentRepository.js";
 import documentLocalsService from "./documentLocalsService.js";
 import documentNaturesService from "./documentNaturesService.js";
+import documentTypesService from "./documentTypesService.js";
 
 const create = async (document) => {
-  if (!document.document_code) {
+  const newDocument = { ...document };
+  if (!newDocument.document_code) {
     throw makeError({ message: "document_code is required", status: 400 });
   }
-  if (!document.name) {
+  if (!newDocument.name) {
     throw makeError({ message: "name is required", status: 400 });
   }
-  if (!document.client_id) {
+  if (!newDocument.client_id) {
     throw makeError({ message: "client_id is required", status: 400 });
   }
-  if (!document.employee_id) {
+  if (!newDocument.employee_id) {
     throw makeError({ message: "employee_id is required", status: 400 });
   }
 
-  if (document.nature_id) {
+  if (newDocument.hasOwnProperty("nature_id")) {
     const natureExists = await documentNaturesService.selectById(
-      document.nature_id
+      newDocument.nature_id
     );
     if (!natureExists) {
       throw makeError({ message: "Nature not found", status: 404 });
     }
   }
 
-  if (document.location_id) {
+  if (newDocument.hasOwnProperty("location_id")) {
     const locationExists = await documentLocalsService.selectById(
-      document.location_id
+      newDocument.location_id
     );
     if (!locationExists) {
       throw makeError({ message: "Location not found", status: 404 });
     }
   }
-  return await documentRepository.create(document);
+
+  if (newDocument.hasOwnProperty("doc_type_id")) {
+    const typeExists = await documentTypesService.selectById(
+      newDocument.doc_type_id
+    );
+    if (!typeExists) {
+      throw makeError({ message: "Type not found", status: 404 });
+    }
+  }
+
+  newDocument.archiving_date = new Date();
+
+  return await documentRepository.create(newDocument);
 };
 
 export default { create };
