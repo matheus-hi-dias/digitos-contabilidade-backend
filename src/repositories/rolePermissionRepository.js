055bea2e37d5ef931ab8016caa55bf224b332521
Matheus Dias <hmatheus031@gmail.com>
2024-04-18 11:15:17 -0300

    ref: employee cant have same permission that role

--- src/repositories/rolePermissionRepository.js
+++ src/repositories/rolePermissionRepository.js
@@ -1,26 +1,32 @@
-import knex from 'knex';
-import knexConfig from '../../knexfile.js';
+import knex from "knex";
+import knexConfig from "../../knexfile.js";
 
 const knexInstance = knex(knexConfig);
 
 const create = async (rolePermission) => {
-  return await knexInstance('role_permission')
+  return await knexInstance("role_permission")
     .insert(rolePermission)
-    .returning('*');
+    .returning("*");
 };
 
 const findByRoleId = async (role_id) => {
-  return await knexInstance('role_permission').select('*').where({ role_id });
+  return await knexInstance("role_permission").select("*").where({ role_id });
 };
 
 const findByPermissionId = async (permission_id) => {
-  return await knexInstance('role_permission')
-    .select('*')
+  return await knexInstance("role_permission")
+    .select("*")
     .where({ permission_id });
 };
 
+const verifyRolePermission = async (role_id, permission_id) => {
+  return await knexInstance("role_permission")
+    .select("*")
+    .where({ role_id, permission_id });
+};
+
 const deleteRolePermission = async (role_id, permission_id) => {
-  return await knexInstance('role_permission')
+  return await knexInstance("role_permission")
     .delete()
     .where({ role_id, permission_id });
 };
@@ -29,5 +35,6 @@ export default {
   create,
   findByRoleId,
   findByPermissionId,
+  verifyRolePermission,
   deleteRolePermission,
 };
--- src/services/employeePermissionsService.js
+++ src/services/employeePermissionsService.js
@@ -2,52 +2,81 @@ import { makeError } from "../middlewares/errorHandler.js";
 import employeePermissionRepository from "../repositories/employeePermissionRepository.js";
 import employeesService from "./employeesService.js";
 import permissionsService from "./permissionsService.js";
+import rolePermissionRepository from "../repositories/rolePermissionRepository.js";
 
 const selectByEmployeeId = async (employee_id) => {
-  const employeePermissions = await employeePermissionRepository.findByEmployeeId(employee_id);
+  const employeePermissions =
+    await employeePermissionRepository.findByEmployeeId(employee_id);
 
   if (employeePermissions.length === 0) {
-    throw makeError({message: "Employee permissions not found", status: 404});
+    throw makeError({ message: "Employee permissions not found", status: 404 });
   }
-  const employeePermissionsFormatted = await Promise.all(employeePermissions.map(async (employeePermission) => {
-    const permissionById = await permissionsService.selectById(employeePermission.permission_id);
-    return permissionById;
-  }))
+  const employeePermissionsFormatted = await Promise.all(
+    employeePermissions.map(async (employeePermission) => {
+      const permissionById = await permissionsService.selectById(
+        employeePermission.permission_id
+      );
+      return permissionById;
+    })
+  );
 
   return employeePermissionsFormatted;
-}
+};
 
 const create = async (employeePermission) => {
   if (!employeePermission.employee_id) {
-    throw makeError({message: "employee_id is required", status: 400});
+    throw makeError({ message: "employee_id is required", status: 400 });
   }
   if (!employeePermission.permission_id) {
-    throw makeError({message: "permission_id is required", status: 400});
+    throw makeError({ message: "permission_id is required", status: 400 });
   }
 
-  const employee = await employeesService.selectById(employeePermission.employee_id);
+  const employee = await employeesService.selectById(
+    employeePermission.employee_id
+  );
   if (!employee) {
-    throw makeError({message: "Employee not found", status: 404});
+    throw makeError({ message: "Employee not found", status: 404 });
   }
 
-  const permission = await permissionsService.selectById(employeePermission.permission_id);
+  const permission = await permissionsService.selectById(
+    employeePermission.permission_id
+  );
   if (!permission) {
-    throw makeError({message: "Permission not found", status: 404});
+    throw makeError({ message: "Permission not found", status: 404 });
+  }
+  console.log({ employee });
+  const permissionExistsOnRole =
+    await rolePermissionRepository.verifyRolePermission(
+      employee.role_id,
+      permission.id
+    );
+
+  if (permissionExistsOnRole.length > 0) {
+    throw makeError({
+      message: "Employee role already have permission",
+      status: 409,
+    });
   }
 
-  const newEmployeePermission = await employeePermissionRepository.create(employeePermission);
+  const newEmployeePermission = await employeePermissionRepository.create(
+    employeePermission
+  );
   return newEmployeePermission[0];
-}
+};
 
 const remove = async (employee_id, permission_id) => {
-  const employeePermission = await employeePermissionRepository.deleteEmployeePermission(employee_id, permission_id);
+  const employeePermission =
+    await employeePermissionRepository.deleteEmployeePermission(
+      employee_id,
+      permission_id
+    );
   if (!employeePermission) {
-    throw makeError({message: "Employee permission not found", status: 404});
+    throw makeError({ message: "Employee permission not found", status: 404 });
   }
-}
+};
 
 export default {
   selectByEmployeeId,
   create,
-  remove
-}
+  remove,
+};
