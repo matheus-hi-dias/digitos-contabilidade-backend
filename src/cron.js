a48791b557d0cdc7a6ea6db99c3753629430a912
Matheus Dias <hmatheus031@gmail.com>
2024-04-18 19:15:17 -0300

    cron changes

--- src/cron.js
+++ src/cron.js
@@ -3,16 +3,16 @@ import knexConfig from "../knexfile";
 
 const knexInstance = knex(knexConfig);
 
-const cron = async () => {
+export async function GET() {
   try {
     console.log("Cron job started", new Date());
-    await knexInstance("doc_nature").select("*");
+    await knexInstance("doc_nature").select("*").limit(1);
     console.log("Cron job finished", new Date());
   } catch (error) {
     console.error(error);
   }
+}
+
+export const config = {
+  runtime: "nodejs",
 };
-
-cron();
-
-export default cron;
